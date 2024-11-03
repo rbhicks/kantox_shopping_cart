3134f48b856c135964d471cdf48dfc99a4bca2d2
Ryan Bryce Hicks <ryan.b@onthegosystems.com>
2024-11-03 10:01:35 -0800

    initial commit

---
+++ lib/kantox_shopping_cart.ex
@@ -0,0 +1,18 @@
+defmodule KantoxShoppingCart do
+  @moduledoc """
+  Documentation for `KantoxShoppingCart`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KantoxShoppingCart.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/kantox_shopping_cart_test.exs
@@ -0,0 +1,8 @@
+defmodule KantoxShoppingCartTest do
+  use ExUnit.Case
+  doctest KantoxShoppingCart
+
+  test "greets the world" do
+    assert KantoxShoppingCart.hello() == :world
+  end
+end
