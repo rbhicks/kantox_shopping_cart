bd1a297ebc4d155f4010f95b60e1f5824505bae1
Ryan Bryce Hicks <ryan.b@onthegosystems.com>
2024-11-03 17:42:42 -0800

    initial commit

---
+++ lib/kantox_shopping_cart_prices.ex
@@ -0,0 +1,16 @@
+defmodule KantoxShoppingCartPrices do
+
+  use GenServer
+  
+  @moduledoc """
+  Default Prices in the following format:
+
+  {product_code, name, price, ISO currency code}
+
+  {GR1, Green Tea, 3.11, GBP}
+  {SR1, Strawberries, 5.00, GBP}
+  {CF1, Coffee, 11.23, GBP}
+  """
+
+  
+end
