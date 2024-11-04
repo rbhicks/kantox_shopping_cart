de0508b5bd7557f94701ee69cf8199d2ce68a745
Ryan Bryce Hicks <ryan.b@onthegosystems.com>
2024-11-03 21:51:57 -0800

    added first test and test scaffolding.

--- test/kantox_shopping_prices_test.exs
+++ test/kantox_shopping_prices_test.exs
@@ -2,7 +2,15 @@ defmodule KantoxShoppingPricesTest do
   use ExSpec
   doctest KantoxShopping.Prices
 
-  # test "greets the world" do
-  #   assert KantoxShoppingCart.hello() == :world
-  # end
+  setup do
+    {:ok, default_prices: Application.fetch_env!(:kantox_shopping, :product_information)}
+  end
+  
+  describe "the kantox shopping prices" do
+    context "checking price veracity" do
+      it "should return the defaults", %{default_prices: default_prices} do
+        assert default_prices == GenServer.call(KantoxShopping.Prices, :get_current_prices)
+      end
+    end
+  end
 end
