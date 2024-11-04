6b6ab9a548db177c8f269942aa1ba84676ab7428
Ryan Bryce Hicks <ryan.b@onthegosystems.com>
2024-11-04 09:29:29 -0800

    first test case described.

--- lib/kantox_shopping_checkout.ex
+++ lib/kantox_shopping_checkout.ex
@@ -11,12 +11,58 @@ defmodule KantoxShopping.Checkout do
   end
 
   def init(_args) do
-    {:ok, []}
+    {:ok, clear_item_codes()}
   end
 
-  def handle_call(:add_item, _from, items) do
+  def handle_cast({:add_item_code, item_code}, item_codes) do
+    {:noreply, item_codes ++ [item_code]}
   end
 
-  def handle_call(:get_total, _from, items) do
+  def handle_cast(:clear_item_codes, item_codes) do
+    {:noreply, clear_item_codes(item_codes)}
   end
+
+  def handle_call(:get_item_codes, _from, item_codes) do
+    {:reply, item_codes, item_codes}
+  end
+
+  def handle_call(:get_total, _from, item_codes) do
+    {:reply, get_total(item_codes), item_codes}
+  end
+
+  defp clear_item_codes(), do: []
+  # just in case in the future clearing has
+  # more complex logic. right now, it just
+  # calls the above.
+  defp clear_item_codes(_item_codes), do: clear_item_codes()
+  
+  defp get_total(item_codes) do
+    product_information = GenServer.call(KantoxShopping.ProductInformation, :get_current_product_information)
+    
+    item_codes
+    |> Enum.frequencies()
+    |> Enum.reduce(0, fn item_code_frequency, total ->
+      total + get_subtotal(item_code_frequency, product_information)
+    end)
+  end
+
+  defp get_subtotal({:gr1, item_code_count}, product_information) when (item_code_count / 2) > 1 do
+    {_, base_price, _} = Map.get(product_information, :gr1)
+    discounted_pairs = item_code_count / 2
+    |> trunc
+
+    # since this function head requires that (item_code_count / 2) > 1
+    # it means we have an odd number. discounted_pairs counts each pair
+    # as one item and then we add one more because it's an add number;
+    # i.e., as we have an odd number, once when account for all the pairs,
+    # an even number, we need to add one more: 2n + 1 is an odd number.
+    (discounted_pairs * base_price) + base_price
+  end
+
+  defp get_subtotal({item_code, 1}, product_information) do
+    {_, base_price, _} = Map.get(product_information, item_code)
+    base_price
+  end
+
+  defp get_subtotal({_item_code, _item_code_count}, _product_information), do: 0
 end
--- test/kantox_shopping_checkout_test.exs
+++ test/kantox_shopping_checkout_test.exs
@@ -2,13 +2,34 @@ defmodule KantoxShoppingCheckoutTest do
   use ExSpec
   doctest KantoxShopping.Checkout
 
-  setup do
+  # setup do
     
-  end
+  # end
   
-  describe "" do
-    context "" do
-      it "" do
+  describe "in the kantox checkout operations" do
+    context "the basic 'add item to basket' operation" do
+      it "should give a list of item codes with the new code added to the end of the list" do
+        GenServer.cast(KantoxShopping.Checkout, :clear_item_codes)
+        GenServer.cast(KantoxShopping.Checkout, {:add_item_code, :gr1})
+        GenServer.cast(KantoxShopping.Checkout, {:add_item_code, :sr1})
+        assert [:gr1, :sr1] == GenServer.call(KantoxShopping.Checkout, :get_item_codes)
+      end
+
+      # it "should fail if the item code is incorrect" do
+      #   assert false
+      # end
+    end
+    
+    context "the offers" do
+      it "Basket: GR1,SR1,GR1,GR1,CF1 should be 22.45" do
+        GenServer.cast(KantoxShopping.Checkout, :clear_item_codes)
+        GenServer.cast(KantoxShopping.Checkout, {:add_item_code, :gr1})
+        GenServer.cast(KantoxShopping.Checkout, {:add_item_code, :sr1})
+        GenServer.cast(KantoxShopping.Checkout, {:add_item_code, :gr1})
+        GenServer.cast(KantoxShopping.Checkout, {:add_item_code, :gr1})
+        GenServer.cast(KantoxShopping.Checkout, {:add_item_code, :cf1})
+        
+        assert 22.45 == GenServer.call(KantoxShopping.Checkout, :get_total)
       end
     end
   end
